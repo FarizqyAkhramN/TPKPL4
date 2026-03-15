87d206ba23fb6e6a7402db6a2d4740c0fab6b791
FarizqyAkhramN <farizkyakhramnugraha@gmail.com>
2026-03-15 19:24:24 +0700

    second commit

--- TP_MODUL4_103022400132/obj/Debug/net10.0/TP_MODUL4_103022400132.AssemblyInfo.cs
+++ TP_MODUL4_103022400132/obj/Debug/net10.0/TP_MODUL4_103022400132.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL4_103022400132")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662e7cfbc7d8744e8cc86a79aa2fbd220abda44a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL4_103022400132")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL4_103022400132")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
